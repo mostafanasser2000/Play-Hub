e0004318126e42835dbac124634249e887becdba
mostafanasser2000 <mustafanasser503@gmail.com>
2024-01-17 00:26:24 +0200

    fix: add settings for templates and static files so template and styles are loaded

--- PlayGrounds/settings.py
+++ PlayGrounds/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = "PlayGrounds.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
